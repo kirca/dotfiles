e2b34dc58a914aa8b196dde2eb172374d5a7173c
Kiril Vangelovski <kiril@lambda-is.com>
2022-11-16 20:32:25 +0100

    update

--- configuration.nix
+++ configuration.nix
@@ -72,6 +72,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Enable postgres
